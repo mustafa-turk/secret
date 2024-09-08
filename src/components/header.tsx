03be91d7a76fd389d60ea9f6e99fd5f05f841ee7
Mustafa Türk <43481284+mustafa-turk@users.noreply.github.com>
2024-09-08 14:02:07 +0200

    good stuff

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { GitIcon } from "@/components/icon";
 
 export default function Header() {
   return (
-    <div className='bg-zinc-950 text-left max-w-[300px] mx-auto pl-4 pr-2 py-2 rounded-full mb-16 flex items-center justify-between'>
+    <div className='bg-zinc-950 text-left max-w-[300px] mx-auto pl-4 pr-2 py-2 rounded-full mb-12 flex items-center justify-between'>
       <p className='font-black text-white text-xl tracking-tight uppercase'>
         Secret
       </p>
