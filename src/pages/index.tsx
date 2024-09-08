f1a50e9226a2c4e810374273e652ee573ee0cb3a
Mustafa Türk <43481284+mustafa-turk@users.noreply.github.com>
2024-09-08 20:21:18 +0200

    good stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,60 +14,57 @@ export default function Home() {
     useSecretGenerator();
 
   return (
-    <div className='p-3 h-screen'>
-      <div className='text-center'>
-        <Header />
-        <span className='font-semibold bg-black bg-opacity-10 p-1 px-2 rounded-full text-xs inline-block mb-4 uppercase tracking-tight'>
-          Open Source
-        </span>
-        <h1 className='text-4xl font-black tracking-tight text-zinc-950 mb-2'>
-          Generate Your Password
-        </h1>
-        <p className='mb-10'>Your secure passwords, only for your eyes.</p>
-        <div className='generator rounded-3xl bg-white p-3 flex flex-col gap-3 backdrop-blur-md bg-white/70 border border-zinc-200'>
-          <div className='flex gap-3'>
-            {Object.keys(SECRET_TYPES).map((type) => (
-              <Button
-                key={type}
-                onClick={() =>
-                  setSecretLength(
-                    SECRET_LENGTH[type as keyof typeof SECRET_LENGTH]
-                  )
-                }
-                selected={
-                  SECRET_LENGTH[type as keyof typeof SECRET_LENGTH] ===
-                  secretLength
-                }
-              >
-                {SECRET_TYPES[type as keyof typeof SECRET_TYPES]}
-              </Button>
-            ))}
+    <div className='p-3 h-screen text-center'>
+      <Header />
+      <span className='font-semibold bg-black bg-opacity-10 p-1 px-2 rounded-full text-xs inline-block mb-4 uppercase tracking-tight'>
+        Open Source
+      </span>
+      <h1 className='text-4xl font-black tracking-tight text-zinc-950 mb-2'>
+        Generate Your Password
+      </h1>
+      <p className='mb-10'>Your secure passwords, only for your eyes.</p>
+      <div className='generator rounded-3xl bg-white p-3 flex flex-col gap-3 backdrop-blur-md bg-white/70 border border-zinc-200'>
+        <div className='flex gap-3'>
+          {Object.keys(SECRET_TYPES).map((type) => (
+            <Button
+              key={type}
+              onClick={() =>
+                setSecretLength(
+                  SECRET_LENGTH[type as keyof typeof SECRET_LENGTH]
+                )
+              }
+              selected={
+                SECRET_LENGTH[type as keyof typeof SECRET_LENGTH] ===
+                secretLength
+              }
+            >
+              {SECRET_TYPES[type as keyof typeof SECRET_TYPES]}
+            </Button>
+          ))}
+        </div>
+        <div className='flex bg-white rounded-2xl p-1 border border-zinc-200 items-center'>
+          <div className='w-full mr-2 relative'>
+            <motion.input
+              key={secret}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 1,
+                ease: "backInOut",
+              }}
+              className='rounded-full w-full text-xl font-semibold pl-2 focus:outline-none bg-white text-zinc-950 disabled:opacity-100 overflow-x-scroll'
+              value={secret + "    "}
+              disabled
+            />
+            <div className='absolute right-0 h-full top-0 w-[30px] bg-gradient-to-r from-transparent to-white' />
           </div>
-          <div className='flex bg-white rounded-2xl p-1 border border-zinc-200 items-center'>
-            <div className='w-full mr-2 relative'>
-              <motion.input
-                key={secret}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{
-                  duration: 1,
-                  ease: "backInOut",
-                }}
-                className='rounded-full w-full text-xl font-semibold pl-2 focus:outline-none bg-white text-zinc-950 disabled:opacity-100 overflow-x-scroll'
-                value={secret + "    "}
-                disabled
-              />
-              <div className='absolute right-0 h-full top-0 w-[30px] bg-gradient-to-r from-transparent to-white' />
-            </div>
-            <Refresh onClick={regenerateSecret} />
-            <Copy value={secret} />
-          </div>
+          <Refresh onClick={regenerateSecret} />
+          <Copy value={secret} />
         </div>
       </div>
       <div className='text-zinc-500 text-center mt-10 text-sm'>
-        Created by{" "}
         <a href='https://mustafaturk.com' target='_blank' rel='noreferrer'>
-          Mustafa Türk
+          Created by Mustafa Türk
         </a>
       </div>
     </div>
