eef4189dce352addb42d18e0006672396afdc1cb
Mustafa Türk <43481284+mustafa-turk@users.noreply.github.com>
2024-12-19 20:59:46 +0100

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,8 @@ import { GitIcon } from "@/components/icon";
 export default function Header() {
   return (
     <div className='bg-zinc-950 text-left max-w-[300px] mx-auto pl-4 pr-2 py-2 rounded-full mb-12 flex items-center justify-between'>
-      <p className='font-black text-white text-xl tracking-tight uppercase'>
-        Secret
+      <p className='font-extrabold text-white text-md tracking-tight uppercase'>
+        secret
       </p>
       <a
         href='https://github.com/mustafa-turk/secret'
