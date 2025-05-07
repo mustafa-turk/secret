a01a355d1eb155a2810ef8f63e1172ebad3f957a
Mustafa Türk <43481284+mustafa-turk@users.noreply.github.com>
2025-05-07 22:14:14 +0200

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
-import { GitIcon } from "@/components/icon";
+import { GitIcon, PasswordIcon } from "@/components/icon";
 
 export default function Header() {
   return (
-    <div className='bg-zinc-950 text-left max-w-[300px] mx-auto pl-4 pr-2 py-2 rounded-full mb-12 flex items-center justify-between'>
-      <p className='font-extrabold text-white text-md tracking-tight uppercase'>
-        secret
+    <div className='bg-zinc-950 text-left max-w-[300px] mx-auto pl-3 pr-2 py-2 rounded-full mb-12 flex items-center justify-between'>
+      <p className='font-extrabold text-white text-md tracking-tight uppercase flex justify-center items-center gap-1'>
+        <PasswordIcon size={21} className='opacity-80' />
       </p>
       <a
         href='https://github.com/mustafa-turk/secret'
--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,7 +1,9 @@
 import { FaGithub } from "react-icons/fa";
 import { GrRefresh } from "react-icons/gr";
 import { PiSpinnerBold } from "react-icons/pi";
+import { FaAsterisk } from "react-icons/fa";
 
 export const GitIcon = FaGithub;
 export const RefreshIcon = GrRefresh;
 export const SpinnerIcon = PiSpinnerBold;
+export const PasswordIcon = FaAsterisk;
