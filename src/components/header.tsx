6fd946f2bd88789069990f0dbcccf196d6a51c98
Mustafa Türk <43481284+mustafa-turk@users.noreply.github.com>
2025-05-12 20:24:16 +0200

    update stuff

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
 import { GitIcon, PasswordIcon } from "@/components/icon";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className='bg-zinc-950 text-left max-w-[300px] mx-auto pl-3 pr-2 py-2 rounded-full mb-12 flex items-center justify-between'>
-      <p className='font-extrabold text-white text-md tracking-tight uppercase flex justify-center items-center gap-1'>
-        <PasswordIcon size={21} className='opacity-80' />
-      </p>
+      <Link href='/'>
+        <p className='font-extrabold text-white text-md tracking-tight uppercase flex justify-center items-center gap-1'>
+          <PasswordIcon size={20} className='opacity-85' />
+        </p>
+      </Link>
+      
       <a
         href='https://github.com/mustafa-turk/secret'
         target='_blank'
